5b1a6ebad24d4328ca043400b27ec4dcff5d6e94
HydroWVU <jbhinkle@gmail.com>
2025-02-08 11:23:05 -0500

    Added TBA key

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "Eyg9MVsOafLgAtdbMHwfQb2MlX3SwWObqQGsR7GexRaOVxDSaq45c0Y0ek2Q594F";
 /**
  * Get list of teams in event
  *
