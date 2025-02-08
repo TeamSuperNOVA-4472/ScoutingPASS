71fd669c77614dc7c16692e760068d44faa907cd
HydroWVU <jbhinkle@gmail.com>
2025-02-08 10:54:58 -0500

    .

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2025vagle",
+      "defaultValue": "2023vagle",
       "required": "true"
     },
     { "name": "Match Level",
